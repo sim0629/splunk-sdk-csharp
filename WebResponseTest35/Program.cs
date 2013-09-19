ce71c19783824e51af6c3af824b468d6b73b7cf6
sgm <sim0629@gmail.com>
2013-09-18 19:51:10 -0700

    Add a project

---
+++ WebResponseTest35/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace WebResponseTest35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AllowAllCertificate();
+            while (true)
+            {
+                Console.ReadLine();
+                //var response = Get("https://192.168.0.11:8089/servicesNS/-/search/configs/conf-savedsearches?count=-1");
+                var response = Get("https://www.google.com");
+                using (var reader = new StreamReader(response.Content))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+        }
+
+        static ResponseMessage Get(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            //request.Headers.Add("Authorization", "Splunk 5bb6cb5d64d1947c4c69375931f87142");
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            return new ResponseMessage(responseStream, response);
+        }
+
+        static void AllowAllCertificate()
+        {
+            if (ServicePointManager.ServerCertificateValidationCallback == null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback =
+                    (sender, certificate, chain, sslPolicyErrors) => true;
+            }
+
+        }
+    }
+}
---
+++ WebResponseTest35/ResponseMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace WebResponseTest35
+{
+    class ResponseMessage
+    {
+        public Stream Content { get; private set; }
+
+        public HttpWebResponse Response { get; private set; }
+
+        public ResponseMessage(Stream content, HttpWebResponse response)
+        {
+            this.Content = content;
+            this.Response = response;
+        }
+
+        #region Bad Finalizer
+        ~ResponseMessage()
+        {
+            if (this.Response != null)
+            {
+                try
+                {
+                    this.Response.Close();
+                }
+                catch (Exception)
+                {
+                    // ignore
+                }
+            }
+        }
+        #endregion
+    }
+}
